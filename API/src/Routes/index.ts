df60696e4fa774a15762d2d5ccc0af0594cbe0aa
HiUlisesOk <uesquivel95@gmail.com>
2023-02-12 13:12:25 -0300

    fixin default path problems

--- API/src/Routes/index.ts
+++ API/src/Routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-import userRoute from "../Routes/user-Routes";
+import userRoute from "../routes/user-routes";
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
